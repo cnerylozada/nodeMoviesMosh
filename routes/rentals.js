dc1f695e29b9a84d4cc006c0624f7b5e08c64e12
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-09-09 09:13:03 -0500

    feat: rental validation done

---
+++ routes/rentals.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { validateRental, Rental } = require("../models/rental");
+const { Customer } = require("../models/customer");
+const { itemWasNotFound } = require("../util/errors");
+const { Movie } = require("../models/movie");
+const router = express.Router();
+
+router.get("/", async (req, res) => {});
+
+router.post("/", async (req, res) => {
+  try {
+    await validateRental(req.body);
+    const customer = await Customer.findById(req.body.customerId).select(
+      "-__v"
+    );
+    if (!customer) return res.status(400).send(itemWasNotFound("customer"));
+    const movie = await Customer.findById(req.body.movieId);
+    if (!movie) return res.status(400).send(itemWasNotFound("movie"));
+    res.status(201).send("new rental was created !");
+  } catch (error) {
+    res.status(400).send(error.errors);
+  }
+});
+
+module.exports = router;
