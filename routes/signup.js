0c5b22ef3477e851fc4cf8af5d5d9a5efdc38904
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-09-11 17:06:02 -0500

    feat: token is sended by cookie

--- routes/signup.js
+++ routes/signup.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const { validateUser, User } = require("../models/user");
+const {
+  createToken,
+  tokenExpirationInSeconds,
+} = require("../util/tokenMethods");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const users = await User.find().select("-__v");
-  return res.status(200).send(users);
+  res.status(200).send(req.cookies);
 });
 
 router.post("/", async (req, res) => {
   try {
     await validateUser(req.body);
     const user = await new User(req.body).save();
+    const token = createToken({ id: user._id, email: user.email });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: tokenExpirationInSeconds * 1000,
+    });
     res.status(201).send(user);
   } catch (error) {
     res.status(400).send(error.errors);
--- util/tokenMethods.js
+++ util/tokenMethods.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const tokenExpirationInSeconds = 3 * 60;
 
 const createToken = (user) => {
-  return jwt.sign(user, "the ninja", {
+  return jwt.sign({ user }, "the ninja", {
     expiresIn: tokenExpirationInSeconds,
   });
 };
