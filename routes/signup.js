fed2d2d185a8a4419060dc0d1feaf48ffdcd3ac7
Cristian <cristian@MacBook-Pro-de-Cristian.local>
2020-09-13 11:33:56 -0500

    feat: email already registered validation

--- routes/signup.js
+++ routes/signup.js
@@ -7,19 +7,24 @@ const {
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.status(200).send(req.cookies);
+  const users = await User.find();
+  res.status(200).send(users);
+  // res.status(200).send(req.cookies);
 });
 
 router.post("/", async (req, res) => {
   try {
     await validateUser(req.body);
+    const isEmailAlreadyExist = await User.find({ email: req.body.email });
+    if (!!isEmailAlreadyExist.length)
+      return res.status(400).send("Email is already registered");
     const user = await new User(req.body).save();
     const token = createToken({ id: user._id, email: user.email });
     res.cookie("jwt", token, {
       httpOnly: true,
       maxAge: tokenExpirationInSeconds * 1000,
     });
-    res.status(201).send(user);
+    res.status(201).send({ id: user._id, email: user.email });
   } catch (error) {
     res.status(400).send(error.errors);
   }
